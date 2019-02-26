3ec9f4d72ee8caa1ca71d666d8e3cb6727cb3d94
Henrik Korsgaard <korsgaard@cs.au.dk>
2019-02-26 09:31:49 +0100

    added client files

--- server/main.go
+++ server/main.go
@@ -4,4 +4,8 @@ import "fmt"
 
 func main() {
 	fmt.Println("Starting server")
+  //I need to make a fileserver for management stuff
+  //I need to create an api for the more complex stuff
+  //this means connecting a proper database -> sqlite or postgresql please. Perhaps sqllite and then just one file per intervention for portability
+
 }
